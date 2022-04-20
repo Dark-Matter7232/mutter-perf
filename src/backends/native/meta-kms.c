216969c1355671dc8479b06d7fe21761d199131b
Daniel van Vugt <daniel.van.vugt@canonical.com>
2022-04-20 18:33:43 +0800

    kms: Fail post_update if shutdown has commenced
    This is necessary with triple buffering because flush_callbacks can
    result in new callbacks being queued, and we would fail the assertion
    in meta_kms_impl_device_atomic_finalize on shutdown.
    
    It's a harmless failure, but still not a good look to SIGABRT. So avoid
    the problem by not queuing new updates when shutdown has already begun.

--- src/backends/native/meta-kms.c
+++ src/backends/native/meta-kms.c
@@ -177,6 +177,8 @@ struct _MetaKms
 
   GList *pending_callbacks;
   guint callback_source_id;
+
+  gboolean shutting_down;
 };
 
 G_DEFINE_TYPE (MetaKms, meta_kms, G_TYPE_OBJECT)
@@ -256,6 +258,9 @@ meta_kms_post_pending_update_sync (MetaKms           *kms,
   MetaKmsFeedback *feedback;
   GList *result_listeners;
   GList *l;
+
+  if (kms->shutting_down)
+    return NULL;
 
   COGL_TRACE_BEGIN_SCOPED (MetaKmsPostUpdateSync,
                            "KMS (post update)");
@@ -752,6 +757,8 @@ prepare_shutdown_in_impl (MetaKmsImpl  *impl,
 void
 meta_kms_prepare_shutdown (MetaKms *kms)
 {
+  kms->shutting_down = TRUE;
+
   meta_kms_run_impl_task_sync (kms, prepare_shutdown_in_impl, NULL, NULL);
   flush_callbacks (kms);
 }
